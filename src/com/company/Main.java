b97bacc3558d0a98a88254254ee99f09b0def57b
Kanakov Kirill <2d25r3@gmail.com>
2016-10-05 15:48:45 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,37 @@
-package com.company;
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+	   String s[]={"Первая строка","А вот как он эту отсортирует 2","строка 3","4 строка","пятая строка","adsfasdfasdfasdfasdfasdfasdfasdf","qwer","qwer2","А"};
+	   sort(s);
+	
 
-public class Main {
+		
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World");
-    }
+	}
+	public static void print(String[] arr){
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+	
+	
+	public static void sort(String[] a){ //метод "Сортировка выбором"
+		int k;
+		String x;
+		
+		for(int  i=0; i < a.length; i++) {   	// i - номер текущего шага
+		    k=i; x=a[i];
+
+		    for(int j=i+1; j < a.length; j++)	// цикл выбора наименьшего элемента
+		      if (a[j].compareTo(x) < 0 ) {
+		        k=j; x=a[j];	        // k - индекс наименьшего элемента
+		      }
+
+		    a[k] = a[i]; a[i] = x;   	// меняем местами наименьший с a[i]
+		  }
+		print(a);
+	}
+
 }
